1abdcfed88514b0a9ea6d138ac8a8dfbc2054d46
Spencer Brendan Kam <sbkam@uci.edu>
2018-05-09 10:24:42 -0700

    Just starting out the code

---
+++ app/src/main/java/edu/uci/assignment3/BoardView.java
@@ -0,0 +1,36 @@
+package edu.uci.assignment3;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class BoardView extends SurfaceView {
+
+    public BoardView(Context c, AttributeSet a, int defStyle){
+        super(c, a, defStyle);
+    }
+
+    public BoardView(Context c, AttributeSet a){
+        super(c, a);
+    }
+
+    public BoardView(Context c){
+        super(c);
+    }
+
+    Bitmap mybitmap= BitmapFactory. decodeResource ( getResources() , R.drawable.ic_launcher_background );
+
+    protected void onDraw ( Canvas c ) {
+        c.drawColor(Color.BLACK); // Set the background to black
+        Rect dst=new Rect () ;
+        dst.set(10 , 30, 20, 40) ; // Set window to place image from (10 ,30) to (20 ,40)
+        c.drawBitmap ( mybitmap , null , dst , null ) ; // Draw the bitmap
+        }
+}
---
+++ app/src/main/java/edu/uci/assignment3/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.uci.assignment3;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
