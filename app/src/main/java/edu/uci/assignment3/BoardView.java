4daa649f61ef2751ef57f980e3389baba379afe8
Spencer Brendan Kam <sbkam@uci.edu>
2018-05-14 20:51:08 -0700

    Nevermind.  I can get 9x9 images displayed now, but they do nothing when clicked on.  Still progess.

--- app/src/main/java/edu/uci/assignment3/BoardView.java
+++ app/src/main/java/edu/uci/assignment3/BoardView.java
@@ -10,12 +10,16 @@ import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.*;//.MotionEvent;
+//import android.view.SurfaceHolder;
+//import android.view.SurfaceView;
 
 public class BoardView extends SurfaceView implements SurfaceHolder.Callback{
 
-    Boolean exists;
+    Boolean exists, clicked;
+    enum direction {up, down, right, left};
+    direction d;
+    int x_initial, y_initial, x_final, y_final, c_width, c_height;
 
     Bitmap mybitmap = BitmapFactory.decodeResource(getResources(), R.drawable.jellybean);
 
@@ -47,8 +51,8 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback{
         c.drawBitmap ( mybitmap , null , dst , null ) ; // Draw the bitmap
 
         Candy[][] candies = new Candy[9][9];
-        int c_width = c.getWidth();
-        int c_height = c.getHeight();
+        c_width = c.getWidth();
+        c_height = c.getHeight();
         for(int x = 0; x < 9; x++){
             for(int y = 0; y < 9; y++){
                 candies[x][y] = new Candy(getResources(), R.drawable.doughnut, x*(c_width/9),y*(c_height/9),c_width/9,c_height/9);
@@ -75,5 +79,60 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback{
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e){
+        if ((e.getAction() == MotionEvent.ACTION_DOWN)&&(!clicked)) {
+            x_initial = (int) e.getX();
+            y_initial = (int) e.getY();
+            clicked = true;  //let program know this is an intial click(candy to be moved)
+
+            x_initial = round(x_initial, true);  //find out what section this click was made in
+            y_initial = round(y_initial, false);
+        }
+
+        else if ((e.getAction() == MotionEvent.ACTION_DOWN)&&clicked) {
+            x_final = (int) e.getX();
+            y_final = (int) e.getY();
+            clicked = false;  //let program know that this is a secondary click(candy to be replaced)
+
+            x_final = round(x_final, true);
+            y_final = round(y_final, false);
+        }
+
+
+
+        return true;
+    }
+
+    //this function returns the section that a click was made at
+    public int round(int x, Boolean horizontal){
+        int sectionX = c_width/9;
+        int sectionY = c_height/9;
+        int initial, dest = 0;
+
+        if(horizontal) {
+            for (int i = 0; i < 9; i++){
+                initial = dest;
+                dest += sectionX;
+                if(x < dest && x > initial){
+                    x = i;
+                    break;
+                }
+            }
+        }
+        else{
+            for (int i = 0; i < 9; i++){
+                initial = dest;
+                dest += sectionY;
+                if(x < dest && x > initial){
+                    x = i;
+                    break;
+                }
+            }
+        }
+
+        return x;
     }
 }
